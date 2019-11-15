c3241320102d97bf94fee3a8599192bd502c394e
Rafał Pocztarski <r.pocztarski@gmail.com>
2019-11-15 23:47:53 +0100

    Update test script

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 echo "TSC loose:\tTSC strict:\tDeno:"
 
+flags=""
+
 for i in noImplicitAny noImplicitThis alwaysStrict strictBindCallApply \
   strictNullChecks strictFunctionTypes strictPropertyInitialization \
   noFallthroughCasesInSwitch noImplicitReturns noUnusedLocals noUnusedParameters # <- not default with --strict
@@ -13,4 +15,12 @@ do
 
   echo "$tl\t\t$ts\t\t$ds\t\t($i)"
 
+  [ "$ds" = ERROR ] && flags="$flags $i"
+
 done
+
+echo "Supported by Deno:   $flags"
+echo "Included by --strict: noImplicitAny noImplicitThis alwaysStrict strictBindCallApply \
+strictNullChecks strictFunctionTypes strictPropertyInitialization"
+echo TSC $(npx tsc -v) Node $(node -v)
+echo $(deno -v)
